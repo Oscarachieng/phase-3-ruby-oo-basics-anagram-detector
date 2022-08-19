9c31fa4eae66ea92b7c27bae2bb3ddd7ba9b3226
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-19 10:30:29 +0300

    commit tree 87bf8603b08da26b45cd798fb66a81fc1479c633

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :my_word
+    def initialize (word)
+        @m_word = word
+    end
+end
+
+wrd = Anagram.new("power")
+puts wrd.match (%[pweper,tick, sneekers,powers])
